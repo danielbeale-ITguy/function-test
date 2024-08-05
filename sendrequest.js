0e697d1e2114a1edd4eaa8607e134cd967f4050b
danielbeale-ITguy <danielbeale719@gmail.com>
2024-08-05 02:44:39 +0000

    indexrequest.js

--- sendrequest.js
+++ sendrequest.js
@@ -1,8 +1,11 @@
-
-
 function updatedcount(){
     const xhttp = new XMLHttpRequest();
     xhttp.open("GET", "https://danssecondtest.azurewebsites.net/api/testingfun?code=ailjqqnYThtm44lVTd5kxxYJU4p-qITeemkDSQB7GzdUAzFuGYSG4Q%3D%3D", true)
-    xhttp.send();
+    xhttp.onreadystatechange = function(){
+        document.getElementById("countlabel").innerHTML = this.responseText;
+    };
+        xhttp.send();
 }
+
+
 
