6e6170bdbaae64cb70c5e0828d5fe7934b2ead6c
danielbeale-ITguy <danielbeale719@gmail.com>
2024-08-05 02:00:57 +0000

    Create sendrequest.js

---
+++ sendrequest.js
@@ -0,0 +1,8 @@
+
+
+function updatedcount(){
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://danssecondtest.azurewebsites.net/api/testingfun?code=ailjqqnYThtm44lVTd5kxxYJU4p-qITeemkDSQB7GzdUAzFuGYSG4Q%3D%3D", true)
+    xhttp.send();
+}
+
